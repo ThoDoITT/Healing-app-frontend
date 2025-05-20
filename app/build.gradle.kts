a74b11ab8404a0c6ab6f0379683d630eb9226f19
anhtt1 <anhtt1@dcv.vn>
2025-05-20 23:55:29 +0700

    fix Ui home & add view select workout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+//   buildFeatures {
+//       viewBinding = true
+//   }
 }
 
 dependencies {
@@ -37,7 +41,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
+
 }
